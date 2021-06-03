5855b83ab25b1a48271c65528ffaf9b322dfb313
MOOLE VENKATA YASWANTH REDDY <84629390+yaswanthmvyr@users.noreply.github.com>
2021-06-03 19:43:09 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -25,13 +25,14 @@ int main(void) {
 }
 float wallis_pi(int x)
 {
-	float a,b,c,pi,i;
-	c=1.0;
-	for(i=1;i<=x;i++){
-		a=4*i*i;
-		b=a-1;
-		c=c*(a/b);
-	}
-	pi=2*c;
+	float p,q,r,pi,i;
+	r=1.0;
+		for(i=1;i<=x;i++)
+		{
+			p=4*i*i;
+			q=p-1;
+			r=r*(p/q);
+		}
+	pi=2*r;
 	return pi;
 }
